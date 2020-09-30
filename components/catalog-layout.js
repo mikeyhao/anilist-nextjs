c793fa3e9102d098875dead19d9e69a7af1efe33
Mikey Hao <mkyhaoxv@gmail.com>
2020-09-30 21:28:34 +0800

    styling added

--- components/catalog-layout.js
+++ components/catalog-layout.js
@@ -2,6 +2,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient, { gql } from "apollo-boost";
 import AnimangaInfo from "./animanga";
 import React, { useState } from 'react';
+import Head from 'next/head';
 
 const Main = () => {
     const [count, setCount] = useState(1);
@@ -23,28 +24,100 @@ const Main = () => {
 
     return (
         <>
-        <span>
+        <Head>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
+        </Head>
+        <main>
+        <div>
             <input 
+                className="searchBar"
+                placeholder="Search here..."
                 onChange = {checkSetQuery}
             />
-        </span>
-        <span>
-            <input 
-                type='number' 
-                onChange={setCountFromInput}
-                min='1'
-                placeholder="jump to page"
-                onFocus={(e)=>e.target.value=""}
-                />
-        </span>
-        <button onClick={()=>count>1?setCount(count-1):setCount(count)}>Back</button>
-        <span>page: {count}</span>
-        <button onClick={()=>setCount(count+1)}>Next</button>
+        </div>
+        <div className="pagingRow">
+            <button 
+                className="back"
+                onClick={()=>count>1?setCount(count-1):setCount(count)}
+                >
+                Back
+            </button>
+            <button 
+                className="next"
+                onClick={()=>setCount(count+1)}>
+                    Next
+            </button>
+            <span>
+                <span className="showPage">Page {count}</span>
+                <input 
+                    className="pageInput"
+                    type='number' 
+                    onChange={setCountFromInput}
+                    min='1'
+                    placeholder="jump to page..."
+                    onFocus={(e)=>e.target.value=""}
+                    />
+            </span>
+        </div>
+        <br></br>
         <div>
             <ApolloProvider client={client}>
                 <AnimangaInfo page={count} search={query}/>
             </ApolloProvider>
         </div>
+        </main>
+        <style jsx>{`
+            main {
+                font-family: 'Poppins', sans-serif;
+            }
+            .searchBar {
+                padding: 8px;
+                display: block;
+                border: none;
+                border-bottom: 1px solid #ccc;
+                width: 98.9%;
+                height: 20px;
+                font-size: 18px;
+                text-align: center;
+                background-color: #e8e8e8;
+                font-family: 'Poppins', sans-serif;
+            }
+            button {
+                border: 1px solid #000;
+                color: white;
+                background-color: #525252;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                padding: 6px 60px;
+                display: inline-block
+                font-family: 'Poppins', sans-serif;
+            }
+            button:hover {
+                color: black;
+                background-color: white;
+            }
+            .pagingRow {
+                margin-bottom: -10px;
+                text-align: center;
+            }
+            .showPage {
+                // background-color: red;
+                margin-left:175px
+            }
+            .pageInput {
+                border: 1px solid #e0e0e0;
+                background-color: white;
+                float: right;
+            }
+            .back {
+                float: left;
+            }
+            .next {
+                float: right;
+            }
+                `}
+                </style>
         </>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -16,23 +16,32 @@ export default function Home() {
     <>
     <Head>
       <title>The Animanga Catalog</title>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
     </Head>
     <main>
-      <div className="headr">Animanga Catalog</div>
+      <div className="headr">
+        <div className="headrTitle">
+        The Animanga Catalog</div>
+      </div>
       <ApolloProvider client={client}>
           <Main />
       </ApolloProvider>
     </main>
-    <footer>
-
-    </footer>
 
     <style jsx>{`
       .headr {
         background-color: black;
         color: white;
         height: 50px;
+        width: 100%;
+      }
+      .headrTitle {
         text-align: center;
+        font-family: 'Poppins', sans-serif;
+        font-size: 20px;
+        margin: auto;
+        padding-top: 10px;
+        width: 50%;
       }
     `}
     </style>
